b094099158529f09c3b8b38d9f14318f693b15f0
Garry Banks <ellisbanks24@gmail.com>
2021-12-09 22:49:10 -0500

    Add project files.

---
+++ WeGotJokes.WebAPI/Areas/HelpPage/ModelDescriptions/SimpleTypeModelDescription.cs
@@ -0,0 +1,6 @@
+namespace WeGotJokes.WebAPI.Areas.HelpPage.ModelDescriptions
+{
+    public class SimpleTypeModelDescription : ModelDescription
+    {
+    }
+}
---
+++ WeGotJokes.WebAPI/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="WeGotJokes.WebAPI.WebApiApplication" Language="C#" %>
