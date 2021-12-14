3ff45871b75da7b43fcd089518a6853fb64dd445
Dillan Groce <groce.dillan@gmail.com>
2021-12-14 18:44:45 -0500

    Animal and Dad Joke Data Layer

--- WeGotJokes.Data/AnimalJoke.cs
+++ WeGotJokes.Data/AnimalJoke.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -8,5 +9,8 @@ namespace WeGotJokes.Data
 {
     public class AnimalJoke : Joke
     {
+        [Key]
+        public int animalJokeId { get; set; }
+        public virtual List<Rating> Ratings { get; set; } = new List<Rating>();
     }
 }
