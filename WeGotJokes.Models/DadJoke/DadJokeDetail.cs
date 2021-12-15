1a1738128a8fedacbe4ab067ec59a8755fd31ef3
Dillan Groce <groce.dillan@gmail.com>
2021-12-14 20:55:23 -0500

    Dad Joke Models

---
+++ WeGotJokes.Models/DadJoke/DadJokeDetail.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WeGotJokes.Models.DadJoke
+{
+    public class DadJokeDetail
+    {
+        public int DadJokeId { get; set; }
+        public string Punchline { get; set; }
+        [Display(Name = "Joke Created")]
+        public DateTimeOffset CreatedUtc { get; set; }
+        [Display(Name =" Joke Last Modified")]
+        public DateTimeOffset ModifiedUtc { get; set; }
+    }
+}
---
+++ WeGotJokes.Models/DadJoke/DadJokeListItem.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WeGotJokes.Models.DadJoke
+{
+    public class DadJokeListItem
+    {
+        public int DadJokeId { get; set; }
+        public string Punchline { get; set; }
+        [Display(Name = "Joke Created")]
+        public DateTimeOffset CreatedUtc { get; set; }
+    }
+}
