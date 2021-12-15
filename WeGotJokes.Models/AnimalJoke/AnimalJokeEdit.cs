bf331f563c35c762c241061b0304c7ecd13d5de0
Dillan Groce <groce.dillan@gmail.com>
2021-12-14 21:01:28 -0500

    AnimalJoke Models

---
+++ WeGotJokes.Models/AnimalJoke/AnimalJokeEdit.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WeGotJokes.Models.AnimalJoke
+{
+    public class AnimalJokeEdit
+    {
+        public int AnimalJokeId { get; set; }
+        public string Punchline { get; set; }
+    }
+}
