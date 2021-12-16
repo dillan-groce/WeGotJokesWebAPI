7ebba622e06769207a2036c745862c3e4b86c3b6
Garry Banks <ellisbanks24@gmail.com>
2021-12-15 21:19:41 -0500

    Completed the DadJokeService assembly

--- WeGotJokes.Services/AnimalJokeService.cs
+++ WeGotJokes.Services/AnimalJokeService.cs
@@ -3,10 +3,12 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using WeGotJokes.Data;
+using WeGotJokes.Models.AnimalJoke;
 
 namespace WeGotJokes.Services
 {
    public class AnimalJokeService
-    {
+    {  
     }
 }
